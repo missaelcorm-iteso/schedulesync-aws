73acbd0e344981bb4a5fb3f4cd677b88d1d91726
Missael Cortes <missaelcorm@gmail.com>
2024-10-27 20:07:53 -0600

    Add required provider 'cloudfare'

--- modules/acm/main.tf
+++ modules/acm/main.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    cloudflare = {
+      source  = "cloudflare/cloudflare"
+      version = "~> 4.0"
+    }
+  }
+}
+
 locals {
   name_prefix = "${var.project}-${var.environment}"
   
