0d94f372c6304d5e68e6d79b6470bcc5b53ba8d2
Missael Cortes <missaelcorm@gmail.com>
2024-11-04 00:14:13 -0600

    Allow terraform destroy alb and s3 resources

--- modules/alb/main.tf
+++ modules/alb/main.tf
@@ -17,7 +17,7 @@ resource "aws_lb" "main" {
   subnets           = var.public_subnet_ids
 
   enable_cross_zone_load_balancing = true
-  enable_deletion_protection = true
+  enable_deletion_protection = false
   enable_http2              = true
   ip_address_type = "ipv4"
 
@@ -131,6 +131,7 @@ resource "aws_lb_listener_rule" "backend" {
 # S3 Bucket for ALB Logs
 resource "aws_s3_bucket" "alb_logs" {
   bucket = "${local.name_prefix}-alb-logs"
+  force_destroy = true
   
   tags = local.common_tags
 }
