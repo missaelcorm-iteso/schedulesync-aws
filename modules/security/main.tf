e3ca133455ef7f35a50f790480ad98430cd15e3d
Missael Cortes <missaelcorm@gmail.com>
2024-11-07 22:12:10 -0600

    Fix aws_iam_role_policy duplicated with backend_task name

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -257,7 +257,7 @@ resource "aws_iam_role_policy" "backend_task" {
   })
 }
 
-resource "aws_iam_role_policy" "backend_task" {
+resource "aws_iam_role_policy" "execution_backend_task" {
   name = "${local.name_prefix}-backend-task-policy"
   role = aws_iam_role.ecs_task_execution_backend.id
 
