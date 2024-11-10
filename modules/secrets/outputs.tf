3725117ad5b8a32a1f80310bf21cf936ce8ae0df
Missael Cortes <missaelcorm@gmail.com>
2024-11-10 12:52:21 -0600

    Release new backend version with support for documentdn

--- modules/secrets/outputs.tf
+++ modules/secrets/outputs.tf
@@ -3,6 +3,11 @@ output "docdb_secrets_manager_secret_arn" {
   value       = aws_secretsmanager_secret.docdb_credentials.arn
 }
 
+output "docdb_secrets_manager_secret_name" {
+  description = "Name of the Secrets Manager secret containing DocumentDB credentials"
+  value       = aws_secretsmanager_secret.docdb_credentials.name
+}
+
 output "docdb_credentials_master_username" {
   description = "Master username for DocumentDB"
   value       = random_string.master_username.result
