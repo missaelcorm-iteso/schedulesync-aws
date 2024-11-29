cbcb8c54b3f56ff19ff90be14cef35fcbbb67098
Sebastián Carrillo <150376468+SebasCarrillo1@users.noreply.github.com>
2024-11-28 20:27:51 -0600

    Fix Critical Vulnerability "Security group rule allows ingress from public internet (HTTPS)."

--- modules/security/main.tf
+++ modules/security/main.tf
@@ -56,7 +56,25 @@ resource "aws_security_group_rule" "alb_https" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
+  # Cloudflare IP ranges
+  # tfsec:ignore:aws-ec2-no-public-ingress-sgr
+  cidr_blocks       = sort([
+    "173.245.48.0/20",
+    "103.21.244.0/22",
+    "103.22.200.0/22",
+    "103.31.4.0/22",
+    "141.101.64.0/18",
+    "108.162.192.0/18",
+    "190.93.240.0/20",
+    "188.114.96.0/20",
+    "197.234.240.0/22",
+    "198.41.128.0/17",
+    "162.158.0.0/15",
+    "104.16.0.0/13",
+    "104.24.0.0/14",
+    "172.64.0.0/13",
+    "131.0.72.0/22"
+  ])
   security_group_id = aws_security_group.alb.id
 }
 
