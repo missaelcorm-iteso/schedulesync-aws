0fccbfb5ff3bd29b35a60c457203693033a28cda
Missael Cortés <missaelcorm@gmail.com>
2025-11-16 14:49:13 -0600

    Restrict S3 bucket, use pre-signed requests for S3 access

--- modules/s3_user_uploads/main.tf
+++ modules/s3_user_uploads/main.tf
@@ -38,9 +38,9 @@ resource "aws_s3_bucket_public_access_block" "user_uploads" {
   bucket = aws_s3_bucket.user_uploads.id
 
   block_public_acls       = true
-  block_public_policy     = false # tfsec:ignore:aws-s3-block-public-policy
+  block_public_policy     = true
   ignore_public_acls      = true
-  restrict_public_buckets = false # tfsec:ignore:aws-s3-no-public-buckets
+  restrict_public_buckets = true
 }
 
 # Bucket policy for public read
