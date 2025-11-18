4fbf21cc7c81ac8234a7b6f3eeec1394d4a37fdc
Missael Cortés <missaelcorm@gmail.com>
2025-11-18 16:42:54 -0600

    Restrict S3 bucket, use pre-signed requests for S3 access

--- modules/s3_user_uploads/main.tf
+++ modules/s3_user_uploads/main.tf
@@ -50,13 +50,6 @@ resource "aws_s3_bucket_policy" "user_uploads" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid       = "PublicReadGetObject"
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = ["s3:GetObject"]
-        Resource  = ["${aws_s3_bucket.user_uploads.arn}/*"]
-      },
-      {
         Sid    = "AllowECSUpload"
         Effect = "Allow"
         Principal = {
